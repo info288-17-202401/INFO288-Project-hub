f90e376f6606fd3c77867559243b1d3779354875
imnotpan <imnotapan@gmail.com>
2024-05-08 06:49:57 -0600

    advance

--- api/src/routes/project_router.py
+++ api/src/routes/project_router.py
@@ -7,19 +7,28 @@ import src.services.project_services as project_services
 
 project_router = APIRouter()
 
-@project_router.get("/create")
+@project_router.post("/create")
 async def create_project(user = Depends(auth_services.get_user_current), 
                           project_data: project_models.ProjectRegisterModel = Depends()):
     await project_services.create_project(project_data, user)
     
+@project_router.get("/auth")
+async def auth_project(user = Depends(auth_services.get_user_current), 
+                        project_data: project_models.ProjectSearchModel = Depends()):
+    token = await auth_services.create_access_token(project_data.project_id)
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
     
+
 @project_router.get("/search")
 async def search_project(user = Depends(auth_services.get_user_current), 
                           project_data: project_models.ProjectSearchModel = Depends()):
-    return await project_services.search_project(project_data)
+    return await project_services.get_project_by_id(project_data.project_id)
 
 
 @project_router.get("/{project_id}/teams")
 async def get_teams_from_project(user = Depends(auth_services.get_user_current), 
                           project_data: project_models.ProjectSearchModel = Depends()):
-    pass
+    return await project_services.get_teams_from_project(project_data)
--- api/src/services/project_services.py
+++ api/src/services/project_services.py
@@ -5,6 +5,10 @@ import src.services.auth_services as auth_services
 import random
 import string
 from fastapi import HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer, OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/project/auth")
+
 
 async def create_project(project_data, user):
     cursor = db.conn.cursor()
@@ -23,7 +27,7 @@ async def create_project(project_data, user):
     cursor.execute(project_add_query,project_add_query_parameters)
     db.conn.commit()
     
-async def search_project(project_data):
+async def project_auth( project_data ):
     find_project = await get_project_by_id(project_data.project_id)
     if not find_project:
          raise HTTPException(status_code = 401, detail="Project not found", 
@@ -31,7 +35,6 @@ async def search_project(project_data):
     if not auth_services.verify_password(project_data.project_password, find_project['project_password']):
         raise HTTPException(status_code = 401, detail="Password error", 
                 headers={"WWW-Authenticate":"Bearer"})  
-    return find_project
 
 async def random_key(tamaño):
     caracteres = string.ascii_letters + string.digits
@@ -48,3 +51,9 @@ async def get_project_by_id(project_id):
         return user_info
     cursor.close()
     return find_project
+
+async def get_teams_from_project(project_id):
+    cursor = db.conn.cursor()
+    teams_search_query = f"SELECT * FROM teams WHERE project_id = '{ project_id }'"
+
+
