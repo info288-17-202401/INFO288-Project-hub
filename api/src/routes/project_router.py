17e2b7034cbad2cc2a4599af9ef8e993e33b35f7
FelipePrieto22 <felipeprieto2321@gmail.com>
2024-05-11 19:34:19 -0400

    fix y return de create_project

--- api/src/routes/project_router.py
+++ api/src/routes/project_router.py
@@ -8,26 +8,41 @@ import src.services.project_services as project_services
 
 project_router = APIRouter()
 
-@project_router.post("/auth", tags = ["project"] , dependencies=[Depends(auth_services.get_user_current)])
-async def auth_project( project_data: project_models.ProjectSearchModel = Depends()):
+
+@project_router.post(
+    "/auth", tags=["project"], dependencies=[Depends(auth_services.get_user_current)]
+)
+async def auth_project(project_data: project_models.ProjectSearchModel = Depends()):
     token = await auth_services.create_access_token(project_data.project_id)
-    data = {
-        "access_token": token,
-        "token_type": "bearer"
-    }
+    data = {"access_token": token, "token_type": "bearer"}
     return data
 
-@project_router.post("/create", tags = ["project"])
-async def create_project( project_data: project_models.ProjectRegisterModel = Depends(), user_data = Depends(auth_services.get_user_current) ):
-    project_id = await project_services.create_project(project_data, user_data)
-    return await project_services.set_profile_in_project(project_id, user_data['app_user_id'], 'admin')
 
-@project_router.get("/{project_auth_key}/teams", tags = ["project"], dependencies=[Depends(auth_services.get_user_current)])
-async def get_teams_from_project( project_auth_key: str):
+@project_router.post("/create", tags=["project"])
+async def create_project(
+    project_data: project_models.ProjectRegisterModel = Depends(),
+    user_data=Depends(auth_services.get_user_current),
+):
+    project_id = await project_services.create_project(project_data, user_data)
+    return await project_services.set_profile_in_project(
+        project_id["project_id"], user_data["app_user_id"], "admin"
+    )
+
+
+@project_router.get(
+    "/{project_auth_key}/teams",
+    tags=["project"],
+    dependencies=[Depends(auth_services.get_user_current)],
+)
+async def get_teams_from_project(project_auth_key: str):
     project = await project_services.get_project_current(project_auth_key)
-    teams = await project_services.get_teams_from_project(project['project_id'])
+    teams = await project_services.get_teams_from_project(project["project_id"])
     return teams
 
-@project_router.get("/{user_id}/projects", tags = ["project"], )
-async def get_projects_from_user( user_data = Depends(auth_services.get_user_current) ):
-    return await project_services.get_all_projects_from_user(user_data['app_user_id'])
+
+@project_router.get(
+    "/{user_id}/projects",
+    tags=["project"],
+)
+async def get_projects_from_user(user_data=Depends(auth_services.get_user_current)):
+    return await project_services.get_all_projects_from_user(user_data["app_user_id"])
--- api/src/services/project_services.py
+++ api/src/services/project_services.py
@@ -10,7 +10,8 @@ from jose import jwt, JWTError
 
 oauth2_scheme_project = OAuth2PasswordBearer(tokenUrl="/project/auth")
 SECRET_KEY = os.environ.get("SECRET_KEY")
-ALGORITHM = 'HS256'
+ALGORITHM = "HS256"
+
 
 async def create_project(project_data, user_data):
     cursor = db.conn.cursor()
@@ -20,34 +21,48 @@ async def create_project(project_data, user_data):
         INSERT INTO project (project_id, project_creation_date, project_description, project_name, project_password, project_owner_id)
         VALUES (%s, %s, %s, %s, %s, %s);
     """
-    project_add_query_parameters = (random_id, 
-                             time_now,
-                             project_data.project_description,
-                             project_data.project_name,
-                             await auth_services.get_password_hash(project_data.project_password),
-                             user_data['app_user_id'])
-    cursor.execute(project_add_query,project_add_query_parameters)
+    project_add_query_parameters = (
+        random_id,
+        time_now,
+        project_data.project_description,
+        project_data.project_name,
+        await auth_services.get_password_hash(project_data.project_password),
+        user_data["app_user_id"],
+    )
+
+    cursor.execute(project_add_query, project_add_query_parameters)
     db.conn.commit()
     return {
         "project_id": random_id,
         "project_description": project_data.project_description,
         "project_name": project_data.project_name,
-        "creation_date": time_now 
+        "creation_date": time_now,
     }
-    
-async def project_auth( project_data ):
+
+
+async def project_auth(project_data):
     find_project = await get_project_by_id(project_data.project_id)
     if not find_project:
-         raise HTTPException(status_code = 401, detail="Project not found", 
-                    headers={"WWW-Authenticate":"Bearer"})  
-    if not auth_services.verify_password(project_data.project_password, find_project['project_password']):
-        raise HTTPException(status_code = 401, detail="Password error", 
-                headers={"WWW-Authenticate":"Bearer"})  
+        raise HTTPException(
+            status_code=401,
+            detail="Project not found",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    if not auth_services.verify_password(
+        project_data.project_password, find_project["project_password"]
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Password error",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return find_project
+
 
 async def random_key(tamaño):
     caracteres = string.ascii_letters + string.digits
-    return ''.join(random.choice(caracteres) for _ in range(tamaño))
+    return "".join(random.choice(caracteres) for _ in range(tamaño))
+
 
 async def get_project_by_id(project_id):
     cursor = db.conn.cursor()
@@ -61,6 +76,7 @@ async def get_project_by_id(project_id):
     cursor.close()
     return find_project
 
+
 async def get_teams_from_project(project_id):
     cursor = db.conn.cursor()
     teams_search_query = f"""
@@ -70,40 +86,54 @@ async def get_teams_from_project(project_id):
     cursor.execute(teams_search_query)
     find_teams = cursor.fetchall()
     column_names = [desc[0] for desc in cursor.description]
-   
+
     teams_as_dict = []
     for team in find_teams:
         team_dict = dict(zip(column_names, team))
         teams_as_dict.append(team_dict)
-    
+
     return teams_as_dict
+
 
 async def get_project_current(project_token: str):
     try:
         token_decode = jwt.decode(project_token, SECRET_KEY, ALGORITHM)
         project_id = token_decode.get("sub")
         if project_id == None:
-            raise HTTPException(status_code = 401, detail="Project id not decoded", 
-            headers={"WWW-Authenticate":"Bearer"})  
+            raise HTTPException(
+                status_code=401,
+                detail="Project id not decoded",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
     except JWTError:
-        raise HTTPException(status_code = 401, detail="Project id not decoded", 
-        headers={"WWW-Authenticate":"Bearer"})  
-    
+        raise HTTPException(
+            status_code=401,
+            detail="Project id not decoded",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
     project = await get_project_by_id(project_id)
     if not project:
-        raise HTTPException(status_code = 401, detail="Project not exists", 
-        headers={"WWW-Authenticate":"Bearer"})  
+        raise HTTPException(
+            status_code=401,
+            detail="Project not exists",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return project
 
-async def set_profile_in_project(project_id, user_id ,profile_type):
+
+async def set_profile_in_project(project_id, user_id, profile_type):
     cursor = db.conn.cursor()
     profile_add_query = f"""
         INSERT INTO app_user_profile_project (app_user_profile_type, app_user_id, project_id)
         VALUES (%s, %s, %s);
     """
     profile_add_query_parameters = (profile_type, user_id, project_id)
-    cursor.execute(profile_add_query,profile_add_query_parameters)
+
+    cursor.execute(profile_add_query, profile_add_query_parameters)
     db.conn.commit()
+    return {"project_id": project_id}
+
 
 async def get_all_projects_from_user(user_id):
     cursor = db.conn.cursor()
@@ -115,10 +145,10 @@ async def get_all_projects_from_user(user_id):
     cursor.execute(projects_search_query, (user_id,))
     find_projects = cursor.fetchall()
     column_names = [desc[0] for desc in cursor.description]
-   
+
     project_as_dict = []
     for projects in find_projects:
         project_dict = dict(zip(column_names, projects))
         project_as_dict.append(project_dict)
-    
+
     return project_as_dict
